be3d2ab50a9fe9f87d760ea02c3f0546b95d196e
annaruiz <anna.ruiz.blazquez@cieep.com>
2023-10-26 14:03:37 +0200

    Initial Commit

---
+++ EjemploDevolverInformacion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EjemploDevolverInformacion
+//
+//  Created by Anna Ruiz on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
